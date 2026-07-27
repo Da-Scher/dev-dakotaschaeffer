ca5458d310b79aeb1744a602cb922ba53467e852
dakotaschaeffer <dak.scher@proton.me>
2026-07-27 09:53:18 -0700

    ProjectsHeaderProps now accepts CommitActivity type list

--- src/components/ProjectsHeader.tsx
+++ src/components/ProjectsHeader.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import {CommitActivity} from "../types/commit";
 
-function ProjectsHeader(): React.JSX.Element {
+interface ProjectsHeaderProps {
+    commitsActivity: CommitActivity[];
+}
+
+function ProjectsHeader({commitsActivity}: ProjectsHeaderProps): React.JSX.Element {
+
     return (
         <>
             <h2>ProjectsHeader</h2>
