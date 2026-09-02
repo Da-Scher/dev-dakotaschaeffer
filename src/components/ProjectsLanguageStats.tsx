631d0f64dde94698d5829801b529e3ac15e908ea
dakotaschaeffer <dak.scher@proton.me>
2026-09-01 23:14:30 -0700

    added piecharts, weekly and hourly activity graphs to LanguageUseStatistics.tsx. Refs #4 and #7.

--- src/components/ProjectsLanguageStats.tsx
+++ src/components/ProjectsLanguageStats.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 import type {CommitActivityResponse} from "../types/commit";
+import LanguagePieChart from "./DataGraphs/LanguagePieChart";
+import "./languageStats.css";
+import HourlyGraph from "./DataGraphs/HourlyGraph";
+import WeeklyGraph from "./DataGraphs/WeeklyGraph";
+import {LanguageStatisticsProvider} from "./DataGraphs/LanguageStatisticsProvider";
 
 export interface ProjectsLanguageStatsProps {
-    commitActivityResponse: CommitActivityResponse;
+    commitActivityResponse: CommitActivityResponse | undefined;
 }
 
 function ProjectsLanguageStats(props: ProjectsLanguageStatsProps): React.JSX.Element {
     const {commitActivityResponse} = props;
 
+    if (!commitActivityResponse) {
+        return <p>Loading commit activities...</p>
+    }
+
     return (
-        <>
-            <p>Projects Language Stats</p>
-        </>
+        <LanguageStatisticsProvider commits={commitActivityResponse.commits}>
+            <div
+                className={"language-stats"}
+            >
+                <HourlyGraph />
+                <WeeklyGraph />
+                <LanguagePieChart />
+            </div>
+        </LanguageStatisticsProvider>
     );
 }
 
